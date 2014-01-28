1cd9ee7bab87351de8d95d01ab24fe563e4cc71e
Shane Sniteman <Shane@Shanes-MacBook-Pro.local>
2014-01-27 19:23:16 -0500

    Initial Commit

---
+++ app/scripts/main.js
@@ -0,0 +1,121 @@
+// Constructor One
+
+function Musician (name,genre) {
+	this.name = name || '?';
+	this.touring = true;
+	this.genre = genre;
+	this.alotexperience = true;
+	this.studiotime = true;
+	this.album = 2;
+
+	this.hireable = function(){
+	this.touring = false;
+	return name + ' is hireable to play'
+	}
+}
+
+var travis = new Musician ("Travis","Country")
+
+travis.hireable()
+
+var justin = new Musician ("Justin","Pop")
+
+var jkwon = new Musician ("Jkwon","Rap")
+
+
+
+// Constructor Two
+
+function MusicVenue (name) {
+	this.name = name || '?';
+	this.stage = 1;
+	this.bathrooms = 2;
+	this.soundguy = true;
+
+	this.nosound = function (){
+	this.soundguy = false;
+	return name + ' has no sound guy';
+	}
+}
+
+var Jakes = new MusicVenue ("Jakes")
+
+var TinRoof = new MusicVenue ("TinRoof")
+
+var Rue77 = new MusicVenue ("Rue77")
+
+Rue77.nosound()
+
+
+// Constructor Three
+
+function Pet (name,type) {
+	this.name = name;
+	this.mammal = true;
+	this.legs = 4;
+	this.cute = true;
+    this.type = type;
+}
+
+var bernard = new Pet("bernard","dog")
+
+var corrie = new Pet("corrie","cat")
+
+var kraken = new Pet("kraken","butterfly")
+kraken.mammal = false
+kraken.legs = 6
+
+
+
+
+
+
+
+
+
+function NflPlayer (name, age){
+this.team = 'colts';
+this.cityLocation = 'indianapolis';
+this.injuredList = false;
+this.name = name || '????';
+this.age = age || '????';
+
+
+this.injury = function(){
+this.injuredList = true;
+return name + ' is now injured!'
+}
+
+}
+
+var andrew = new NflPlayer ('Andrew Luck', 24);
+
+var reggie = new NflPlayer ('Reggie Wayne');
+
+var trent = new NflPlayer ('Trent Richardson');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
